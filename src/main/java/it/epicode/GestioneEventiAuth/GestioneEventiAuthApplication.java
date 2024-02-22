4d81211bc30395fec88fc503eeafb4ebace9fb56
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-02-22 19:52:31 +0100

    Initial commit

---
+++ src/main/java/it/epicode/GestioneEventiAuth/GestioneEventiAuthApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.GestioneEventiAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/GestioneEventiAuth/GestioneEventiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.GestioneEventiAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
